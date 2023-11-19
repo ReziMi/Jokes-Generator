3bd4445eb1f7542de2e7c36fb2d13ecea4d22d9f
ReziMi <revaz.mikadze92@gmail.com>
2023-11-19 21:03:38 +0400

    update joke property to setup + delivery

--- script.js
+++ script.js
@@ -6,23 +6,21 @@ const url = "https://v2.jokeapi.dev/joke/Any";
 async function getJoke() {
     try {
         const response = await fetch(url);
-        
+
         // Check if the response status is OK
         if (!response.ok) {
             throw new Error(`Failed to fetch joke. Status: ${response.status}`);
         }
 
-        const { joke } = await response.json();
+        const { setup, delivery } = await response.json();
 
-        if (joke === undefined) {
-            jokeTxt.innerText = "Tap for a redo as joke was not found in the API response!\nJokes: mild.\nBackground-image: wild. \uD83D\uDE0A\nThoughts?";
+        if (delivery === undefined || setup === undefined) {
+            jokeTxt.innerText = "Tap for a redo as the joke was not found in the API response!\nJokes: mild.\nBackground-image: wild. \uD83D\uDE0A\nThoughts?";
             throw new Error('Joke not found in the API response.');
         }
 
-        jokeTxt.innerText = joke;
+        jokeTxt.innerText = setup + " " + delivery;
 
-        // Log the joke after it has been fetched and assigned
-        console.log(joke);
     } catch (error) {
         console.error('Error fetching joke:', error.message);
     }
